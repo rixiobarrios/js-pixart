00756bb5f3e3f6a0bc56a2444109563eaac600f0
rixiobarrios <rixiobarrios.consulado@gmail.com>
2019-12-28 12:11:10 -0600

    First draft of homework

--- pixart.js
+++ pixart.js
@@ -1,1 +1,43 @@
+//Commit 1
+// //When I click the "Set Color" button, it should change the color of the "brush" box to the color I specify in the input field.
+// You can use document.querySelector(or another document method) to select the element, then add an event listener.
 
+let brush = document.querySelector('.brush');
+let setColor = document.querySelector('#form');
+
+// HINT: You will notice that the page refreshes whenever you click the button.You need to prevent this from happening using a method you have not used before.Google "javascript event prevent default".You can also reference this portion of the Events & Callbacks lesson plan.
+
+setColor.addEventListener('click', function(event) {
+    console.log('click');
+    event.preventDefault();
+});
+
+//Commit 2
+//The same thing should happen when I press the enter key from inside the input field
+
+setColor.addEventListener('keyup', function(event) {
+    console.log('keyup');
+    event.preventDefault();
+});
+//Commit 3
+// Create 20 divs of the "square" class and append them to the body
+// Hint: use.appendChild()
+
+for (let i = 0; i < 8000; i++) {
+    let newDiv = document.createElement('div');
+    newDiv.classList.add('square');
+    document.getElementsByTagName('body')[0].appendChild(newDiv);
+}
+
+//Commit 4
+// Add functionality so that when I click on each "square", it changes the color of that individual square to "green"
+// Hint: either add the event listener while creating the squares, or listen for events on the body element
+
+//Commit 5
+//Modify your code so that when I click on each "square", it changes to the color I set using my input instead of "green" every time.
+
+//Commit 6
+//Modify the CSS so that the "square" class has a height and width of 10px and a margin of 0.
+// Modify your code so that you are creating 8000 divs instead of 20.
+// Change the event that changes your box colors from 'click' to 'mouseover'
+// Paint a picture!
